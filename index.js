cb425ca7ca6b31b6b4fceaeae2bf7e256047d04a
Zeddzuruzuranda <raphomelvo@gmail.com>
2022-08-31 13:51:16 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", changeText)
+function changeText() {
+    let newText = document.getElementById("text");
+    newText.textContent = "This is really cool!";
+} 
